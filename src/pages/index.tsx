92a4a2d023c858c337e2f5235982f3306537adf9
Neander de Souza <neander.faria@hotmail.com>
2023-01-30 16:04:37 -0300

    Estruturando o Formulário

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,31 @@
-export default function Home() {
+import Link from 'next/link';
+
+import styles from '../styles/Formulario.module.css';
+
+import Cartao from '../components/Cartao';
+
+export default function Formulario() {
   return (
-    <div style={{ display: 'flex' }}>
-      <h1>Formulário de Início</h1>
+    <div className={styles.formulario}>
+      <div>
+        <Cartao bgcolor="#c0392c">
+          <h1>Monty Hall</h1>
+        </Cartao>
+
+        <Cartao></Cartao>
+      </div>
+
+      <div>
+        <Cartao></Cartao>
+
+        <Cartao bgcolor="#28a085">
+          <Link href={`/jogo/4/2`} className={styles.link}>
+            <h2>
+              Iniciar
+            </h2>
+          </Link>
+        </Cartao>
+      </div>
     </div>
   )
 }
