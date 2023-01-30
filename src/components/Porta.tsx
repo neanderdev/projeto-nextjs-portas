aa08ad0ed1abf2aab85aa56551fc7074d7473c69
Neander de Souza <neander.faria@hotmail.com>
2023-01-30 15:01:30 -0300

    Adicionando Eventos a Porta #03

--- src/components/Porta.tsx
+++ src/components/Porta.tsx
@@ -12,18 +12,37 @@ interface PortaProps {
 export default function Porta(props: PortaProps) {
     const porta = props.value;
 
-    const selecionada = porta.selecionada ? styles.selecionada : '';
+    const selecionada = porta.selecionada && !porta.aberta ? styles.selecionada : '';
 
     const alternarSelecao = (event: MouseEvent<HTMLDivElement>) => props.onChange(porta.alternarSelecao());
+    const abrir = (event: MouseEvent<HTMLDivElement>) => {
+        event.stopPropagation();
+
+        props.onChange(porta.abrir());
+    };
+
+    function renderizarPorta() {
+        return (
+            <div className={styles.porta}>
+                <div className={styles.numero}>{porta.numero}</div>
+
+                <div
+                    className={styles.macaneta}
+                    onClick={abrir}
+                ></div>
+            </div>
+
+        );
+    }
 
     return (
         <div className={styles.area} onClick={alternarSelecao}>
             <div className={`${styles.estrutura} ${selecionada}`}>
-                <div className={styles.porta}>
-                    <div className={styles.numero}>{porta.numero}</div>
-
-                    <div className={styles.macaneta}></div>
-                </div>
+                {
+                    porta.aberta
+                        ? false
+                        : renderizarPorta()
+                }
             </div>
 
             <div className={styles.chao}></div>
