19fbeec0ce59a0be491f4ff1cc7c61bf49b1cdf9
Neander de Souza <neander.faria@hotmail.com>
2023-01-30 15:30:11 -0300

    Componente Jogo #01

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,7 @@
-import { useState } from 'react';
-
-import Porta from '../components/Porta';
-
-import { atualizarPortas, criarPortas } from '../functions/portas';
-
 export default function Home() {
-  const [portas, setPortas] = useState(criarPortas(4, 3));
-
-  function renderizarPortas() {
-    return portas.map(porta => {
-      return <Porta
-        key={porta.numero}
-        value={porta}
-        onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}
-      />
-    });
-  }
-
   return (
     <div style={{ display: 'flex' }}>
-      {renderizarPortas()}
+      <h1>Formulário de Início</h1>
     </div>
   )
 }
---
+++ src/pages/jogo.tsx
@@ -0,0 +1,33 @@
+import { useState } from 'react';
+
+import styles from '../styles/Jogo.module.css';
+
+import Porta from '../components/Porta';
+
+import { atualizarPortas, criarPortas } from '../functions/portas';
+
+export default function Jogo() {
+    const [portas, setPortas] = useState(criarPortas(10, 2));
+
+    function renderizarPortas() {
+        return portas.map(porta => {
+            return <Porta
+                key={porta.numero}
+                value={porta}
+                onChange={novaPorta => setPortas(atualizarPortas(portas, novaPorta))}
+            />
+        });
+    }
+
+    return (
+        <div id={styles.jogo}>
+            <div className={styles.portas}>
+                {renderizarPortas()}
+            </div>
+
+            <div className={styles.botoes}>
+                
+            </div>
+        </div>
+    );
+}
